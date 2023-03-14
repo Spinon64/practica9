7b0fce2721377ff1baeeec47c17ec3f1b21ccdab
Spinon64 <90027475+Spinon64@users.noreply.github.com>
2023-03-14 15:59:54 -0600

    push

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => { // route handler 
+    res.send("Hello World, this is the root route");
+});
+
+
+app.get("/uno", (req, res)=> { //route handler
+    res.send("Hello World, from route One");
+});
+
+
+app.get("/prueba", (req, res) => { // route handler
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Document</h1>
+    </body>
+    </html>`);
+});
+
+app.listen(3000);
